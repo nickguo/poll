fd1ab5e4f9e98f84013a999dffa0bec021dd56c1
Rishi Dhanaraj <rishid97@gmail.com>
2015-10-03 04:46:14 -0700

    probably doesn't work, added timeout, listens for votes

--- poll.js
+++ poll.js
@@ -1,19 +1,40 @@
 $(document).ready(function() {
   var socket = io();
+  var activePolls;
+  var inactivePolls;
 
-  $('#click-box').click(function(){
-      console.log('clicked');
-      socket.emit('vote', 'user voted');
-  });
+  $('#newPoll').click(function(){
+    var pollData = 
+      {
+        'id': socket.id,
+        'title': $('#pollName').val(),
+        'options': {
+          'ans 1': 0,
+          'ans 2': 0
+        }
+      };
+    socket.emit('new_poll', pollData);
+  }
+  
+  socket.on('new_poll', newPoll){
+    $("#activePolls").append('<div class=activePollMenu">'
+        + newPoll['title'] + '</div');
+  }
 
-  socket.on('vote', function(msg){
+  socket.on('update_vote', function(msg){
       console.log('got clicked');
       var currentSum = parseInt($('#target').html());
       $('#target').html(currentSum + 1);
   });
 
   socket.on('current_polls', function(currentPolls){
-      console.log('currentPolls: ' + currentPolls);
+    activePolls = currentPolls['activePolls'];
+    inactivePolls = currentPolls['inactivePolls'];
+    for(poll in activePolls){
+      $("#activePolls").append('<div class="activePollMenu">' 
+            + activePolls[poll]['title'] 
+            + '</div>');
+    }
   });
 
 });
