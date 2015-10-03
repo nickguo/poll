4ff573d906dc4310d821ccd637b5ed6896025da2
Andrew Wang <zepianistandrew@gmail.com>
2015-10-03 11:09:27 -0700

    Tab structure menu

--- poll.js
+++ poll.js
@@ -3,6 +3,32 @@ $(document).ready(function() {
   var activePolls;
   var inactivePolls;
 
+  var activeTab = $('#selectActive');
+  
+  $('#selectActive').click(function(){
+    activeTab.css("background-color", "white");
+    activeTab.css("color", "black");
+    $('#selectActive').css("background-color", "black");
+    $('#selectActive').css("color", "white");
+    activeTab = $("#selectActive");
+  });
+
+  $('#selectInactive').click(function(){
+    activeTab.css("background-color", "white");
+    activeTab.css("color", "black");
+    $('#selectInactive').css("background-color", "black");
+    $('#selectInactive').css("color", "white");
+    activeTab = $("#selectInactive");
+  });
+  $('#createNewPoll').click(function(){
+    activeTab.css("background-color", "white");
+    activeTab.css("color", "black");
+    $('#createNewPoll').css("background-color", "black");
+    $('#createNewPoll').css("color", "white");
+    activeTab = $("#createNewPoll");
+    $('#newPollMenu').css("display", "inline-block");
+    $('#newPollMenu').css("height", "100px");
+  });
   $('#newPoll').click(function(){
     document.getElementById('newPollForm').style.display = "flex";
     var pollData = 
