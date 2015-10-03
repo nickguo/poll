4f64e03de4f51935388f58d39112a0af0650f85f
Joshua Chao <joshua.chao39@gmail.com>
2015-10-03 04:52:36 -0700

    Modified css and html to basic structure.

--- poll.js
+++ poll.js
@@ -3,24 +3,33 @@ $(document).ready(function() {
   var activePolls;
   var inactivePolls;
 
+
+  function showNewPollForm() {
+    console.log("Showing new poll form.");
+    document.getElementById('newPollForm').style.display = "block";
+  }
+
+
   $('#click-box').click(function(){
       console.log('clicked');
       socket.emit('vote', 'user voted');
   });
 
   $('#newPoll').click(function(){
+    console.log("Showing new poll form.");
+    document.getElementById('newPollForm').style.display = "flex";
     var pollData = 
       {
         'name': $('#pollName').val(),
         'options': []
       };
     socket.emit('new_poll', pollData);
-  }
+  });
   
-  socket.on('new_poll', newPoll){
+  socket.on('new_poll', function(newPoll) {
     $("#activePolls").append('<div class=activePollMenu">'
         + newPoll['title'] + '</div');
-  }
+  });
 
   socket.on('vote', function(msg){
       console.log('got clicked');
